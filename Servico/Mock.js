971418e8e171437874654197307b1b76348b176e
victortads <vgs8@discente.ifpe.edu.br>
2024-09-06 14:13:32 -0300

    style: ajustes de css de serviços

--- Servico/Mock.js
+++ Servico/Mock.js
@@ -6,155 +6,155 @@ let data;
 
 
 
-data ={
-  "seguranca": [
-    {
-        "nome": "Delegacia da Mulher",
-        "link": "https://www.sds.pe.gov.br/seguranca-publica/delegacia-da-mulher"
-    },
-    {
-        "nome": "Centro de Referencia de Atendimento à Mulher em Situação de Violencia (CRAM)",
-        "link": "https://www.sds.pe.gov.br/seguranca-publica/centro-de-referencia"
-    },
-    {
-        "nome": "ServiÃ§o de Atendimento Ã  VÃ­tima de ViolÃªncia (SAVV)",
-        "link": "https://www.sds.pe.gov.br/seguranca-publica/savv"
-    }
-],
-"assistencia_social": [
-    {
-        "nome": "Centro de ReferÃªncia em AssistÃªncia Social (CRAS)",
-        "link": "https://www.sigas.pe.gov.br/pagina/centro-de-referncia-de-assistncia-social--cras"
-    },
-    {
-        "nome": "Centro de ReferÃªncia Especializado de AssistÃªncia Social (CREAS)",
-        "link": "https://www.sigas.pe.gov.br/pagina/centro-especializado-de-assistncia-social-municipal--creas-municipal"
-    },
-    {
-        "nome": "Programa Brotar",
-        "link": "https://portal.mppe.mp.br/w/mppe-institui-programa-de-contrata%C3%A7%C3%A3o-de-mulheres-em-situa%C3%A7%C3%A3o-vulner%C3%A1vel"
-    },
-    {
-        "nome": "Pernambuco Sem Fome",
-        "link": "https://www.sigas.pe.gov.br/noticia/pernambuco-sem-fome-e-com-acolhimento--mulheres"
-    }
-],
-"saude": [
-    {
-        "nome": "Hospital da Mulher do Recife",
-        "link": "https://www2.recife.pe.gov.br/pagina/hospital-da-mulher-do-recife-dra-merces-pontes-cunha"
-    },
-    {
-        "nome": "Programa de SaÃºde da Mulher",
-        "link": "https://www.saude.pe.gov.br/saude-da-mulher"
-    },
-    {
-        "nome": "Centro de SaÃºde da Mulher do Recife",
-        "link": "https://www.recife.pe.gov.br/centro-de-saude-da-mulher"
-    },
-    {
-        "nome": "Programa de Apoio Ã  Maternidade",
-        "link": "https://www.saude.pe.gov.br/apoio-maternidade"
-    }
-],
-"trabalho_emprego": [
-    {
-        "nome": "Centro de Apoio ao Trabalhador (CAT)",
-        "link": "https://www.seteq.pe.gov.br/cat/"
-    },
-    {
-        "nome": "Programa de InclusÃ£o Produtiva para Mulheres",
-        "link": "https://www.mds.gov.br/inclusao-produtiva"
-    },
-    {
-        "nome": "AgÃªncia do Trabalho - Mulheres",
-        "link": "https://www.seteq.pe.gov.br/"
-    }
-],
-"assistencia_juridica": [
-    {
-        "nome": "Programa de OrientaÃ§Ã£o JurÃ­dica para Mulheres",
-        "link": "https://www.oab-pe.org.br/orientacao-juridica"
-    },
-    {
-        "nome": "Defensoria PÃºblica - Atendimento Ã s Mulheres",
-        "link": "https://www.defensoria.pe.gov.br/"
-    }
-],
-"educacao_capacitacao": [
-    {
-        "nome": "Centro de FormaÃ§Ã£o e CapacitaÃ§Ã£o para Mulheres",
-        "link": "https://www.sedh.pe.gov.br/formacao-capacitacao/"
-    },
-    {
-        "nome": "Programa Bora Empreender",
-        "link": "https://www.sedepe.pe.gov.br/index.php?option=com_content&view=article&id=7941&Itemid=254"
-    }
-],
-"servicos_gerais": [
-    {
-        "nome": "Central de Atendimento ao CidadÃ£o",
-        "link": "https://www.cidadania.pe.gov.br/"
-    },
-    {
-        "nome": "Secretaria da Mulher",
-        "link": "http://www2.secmulher.pe.gov.br/web/secretaria-da-mulher"
-    },
-    {
-        "nome": "Programa de ProteÃ§Ã£o e Atendimento Ã s Mulheres",
-        "link": "https://www.sedh.pe.gov.br/"
-    }
-],
-"violencia_vulnerabilidade": [
-    {
-        "nome": "Centro de Atendimento a Mulher em SituaÃ§Ã£o de Violecia Domestica",
-        "link": "https://www.sds.pe.gov.br/seguranca-publica/centro-atendimento"
-    },
-    {
-        "nome": "Rede de Atendimento Ã  Mulher em SituaÃ§Ã£o de Vulnerabilidade",
-        "link": "https://www.sds.pe.gov.br/seguranca-publica/rede-vulnerabilidade"
-    },
-    {
-        "nome": "Por Elas Para Elas",
-        "link": "https://portal.tjpe.jus.br/-/-por-elas-para-elas-programa-acolhe-mulheres-do-judiciario-vitimas-de-violencia-domestica"
-    }
-],
-"empoderamento": [
-    {
-        "nome": "Programa de Empoderamento Feminino",
-        "link": "https://www.sedh.pe.gov.br/empoderamento-feminino/"
-    },
-    {
-        "nome": "Instituto de Mulheres Empreendedoras",
-        "link": "https://www.mulherempreendedora.pe.gov.br/"
-    }
-],
-"servicos_saude_mental": [
-    {
-        "nome": "Centro de ReferÃªncia em SaÃºde Mental (CRSM) - Mulheres",
-        "link": "https://www.saude.pe.gov.br/saude-mental"
-    },
-    {
-        "nome": "Programa de SaÃºde Mental para Mulheres",
-        "link": "https://www.saude.pe.gov.br/programas/saude-mental"
-    }
-],
-"direitos_protecao": [
-    {
-        "nome": "Delegacia de ProteÃ§Ã£o aos Direitos da Mulher",
-        "link": "https://www.sds.pe.gov.br/seguranca-publica/delegacia-direitos"
-    },
-    {
-        "nome": "ComissÃ£o de Direitos Humanos e Cidadania",
-        "link": "https://www.cidadania.pe.gov.br/"
-    },
-    {
-        "nome": "Rede de ProteÃ§Ã£o Ã  Mulher",
-        "link": "https://www.secmulher.pe.gov.br/rede-protecao"
-    }
-]
+data = {
+    "seguranca": [
+        {
+            "nome": "Delegacia da Mulher",
+            "link": "https://www.sds.pe.gov.br/seguranca-publica/delegacia-da-mulher"
+        },
+        {
+            "nome": "Centro de Referência de Atendimento à Mulher em Situação de Violência (CRAM)",
+            "link": "https://www.sds.pe.gov.br/seguranca-publica/centro-de-referencia"
+        },
+        {
+            "nome": "Serviço de Atendimento à Vítima de Violência (SAVV)",
+            "link": "https://www.sds.pe.gov.br/seguranca-publica/savv"
+        }
+    ],
+    "assistencia_social": [
+        {
+            "nome": "Centro de Referência em Assistência Social (CRAS)",
+            "link": "https://www.sigas.pe.gov.br/pagina/centro-de-referncia-de-assistncia-social--cras"
+        },
+        {
+            "nome": "Centro de Referência Especializado de Assistência Social (CREAS)",
+            "link": "https://www.sigas.pe.gov.br/pagina/centro-especializado-de-assistncia-social-municipal--creas-municipal"
+        },
+        {
+            "nome": "Programa Brotar",
+            "link": "https://portal.mppe.mp.br/w/mppe-institui-programa-de-contrata%C3%A7%C3%A3o-de-mulheres-em-situa%C3%A7%C3%A3o-vulner%C3%A1vel"
+        },
+        {
+            "nome": "Pernambuco Sem Fome",
+            "link": "https://www.sigas.pe.gov.br/noticia/pernambuco-sem-fome-e-com-acolhimento--mulheres"
+        }
+    ],
+    "saude": [
+        {
+            "nome": "Hospital da Mulher do Recife",
+            "link": "https://www2.recife.pe.gov.br/pagina/hospital-da-mulher-do-recife-dra-merces-pontes-cunha"
+        },
+        {
+            "nome": "Programa de Saúde da Mulher",
+            "link": "https://www.saude.pe.gov.br/saude-da-mulher"
+        },
+        {
+            "nome": "Centro de Saúde da Mulher do Recife",
+            "link": "https://www.recife.pe.gov.br/centro-de-saude-da-mulher"
+        },
+        {
+            "nome": "Programa de Apoio à Maternidade",
+            "link": "https://www.saude.pe.gov.br/apoio-maternidade"
+        }
+    ],
+    "trabalho_emprego": [
+        {
+            "nome": "Centro de Apoio ao Trabalhador (CAT)",
+            "link": "https://www.seteq.pe.gov.br/cat/"
+        },
+        {
+            "nome": "Programa de Inclusão Produtiva para Mulheres",
+            "link": "https://www.mds.gov.br/inclusao-produtiva"
+        },
+        {
+            "nome": "Agência do Trabalho - Mulheres",
+            "link": "https://www.seteq.pe.gov.br/"
+        }
+    ],
+    "assistencia_juridica": [
+        {
+            "nome": "Programa de Orientação Jurídica para Mulheres",
+            "link": "https://www.oab-pe.org.br/orientacao-juridica"
+        },
+        {
+            "nome": "Defensoria Pública - Atendimento às Mulheres",
+            "link": "https://www.defensoria.pe.gov.br/"
+        }
+    ],
+    "educacao_capacitacao": [
+        {
+            "nome": "Centro de Formação e Capacitação para Mulheres",
+            "link": "https://www.sedh.pe.gov.br/formacao-capacitacao/"
+        },
+        {
+            "nome": "Programa Bora Empreender",
+            "link": "https://www.sedepe.pe.gov.br/index.php?option=com_content&view=article&id=7941&Itemid=254"
+        }
+    ],
+    "servicos_gerais": [
+        {
+            "nome": "Central de Atendimento ao Cidadão",
+            "link": "https://www.cidadania.pe.gov.br/"
+        },
+        {
+            "nome": "Secretaria da Mulher",
+            "link": "http://www2.secmulher.pe.gov.br/web/secretaria-da-mulher"
+        },
+        {
+            "nome": "Programa de Proteção e Atendimento às Mulheres",
+            "link": "https://www.sedh.pe.gov.br/"
+        }
+    ],
+    "violencia_vulnerabilidade": [
+        {
+            "nome": "Centro de Atendimento à Mulher em Situação de Violência Doméstica",
+            "link": "https://www.sds.pe.gov.br/seguranca-publica/centro-atendimento"
+        },
+        {
+            "nome": "Rede de Atendimento à Mulher em Situação de Vulnerabilidade",
+            "link": "https://www.sds.pe.gov.br/seguranca-publica/rede-vulnerabilidade"
+        },
+        {
+            "nome": "Por Elas Para Elas",
+            "link": "https://portal.tjpe.jus.br/-/-por-elas-para-elas-programa-acolhe-mulheres-do-judiciario-vitimas-de-violencia-domestica"
+        }
+    ],
+    "empoderamento": [
+        {
+            "nome": "Programa de Empoderamento Feminino",
+            "link": "https://www.sedh.pe.gov.br/empoderamento-feminino/"
+        },
+        {
+            "nome": "Instituto de Mulheres Empreendedoras",
+            "link": "https://www.mulherempreendedora.pe.gov.br/"
+        }
+    ],
+    "servicos_saude_mental": [
+        {
+            "nome": "Centro de Referência em Saúde Mental (CRSM) - Mulheres",
+            "link": "https://www.saude.pe.gov.br/saude-mental"
+        },
+        {
+            "nome": "Programa de Saúde Mental para Mulheres",
+            "link": "https://www.saude.pe.gov.br/programas/saude-mental"
+        }
+    ],
+    "direitos_protecao": [
+        {
+            "nome": "Delegacia de Proteção aos Direitos da Mulher",
+            "link": "https://www.sds.pe.gov.br/seguranca-publica/delegacia-direitos"
+        },
+        {
+            "nome": "Comissão de Direitos Humanos e Cidadania",
+            "link": "https://www.cidadania.pe.gov.br/"
+        },
+        {
+            "nome": "Rede de Proteção à Mulher",
+            "link": "https://www.secmulher.pe.gov.br/rede-protecao"
+        }
+    ]
 }
 
-  
 
-  export default data
+
+export default data
